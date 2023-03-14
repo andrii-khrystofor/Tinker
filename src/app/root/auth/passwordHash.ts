940654e3582d2fad74a4ad3527f1eabc29abe013
Chernov <ihsdigu.g@gmail.com>
2023-03-14 10:47:28 +0200

    Changed hashing from browser API to bcrypt

--- src/app/root/auth/passwordHash.ts
+++ src/app/root/auth/passwordHash.ts
@@ -1,13 +1,6 @@
+import * as bcrypt from 'bcryptjs';
+
 export async function hashPassword(password: string) {
-    if (!window.crypto.subtle) {
-        console.error('Web Crypto API not supported');
-        return;
-    }
-  
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await window.crypto.subtle.digest('SHA-256', data) 
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
 }
