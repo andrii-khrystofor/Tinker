679db8dbc895653a00632ce7a3accdfacde14000
Vlad <103422523+JustVimeR@users.noreply.github.com>
2023-03-15 15:43:15 +0200

    Щось типу Added Login Page (#7)
    * Щось типу Added Login Page
    
    * Fixed Comments
    
    * New Fix 2

--- src/app/root/auth/login/login.component.ts
+++ src/app/root/auth/login/login.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Subject} from 'rxjs';
+import { InputTypes } from 'src/app/types/enums/input-types.enum';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent{
+  
+  LoginForm: FormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+  });
+
+  passwordInput = InputTypes.Password;
+
+  constructor( private router: Router) { 
+  }
+ 
+  submitData() {
+    this.LoginForm.markAllAsTouched();
+    }
+
+backToSignIn() {
+  this.router.navigate([
+    '/root',
+    {
+      outlets: {
+        modalOutlet: [
+          'modal',
+          'auth',
+          'sign-in',
+        ],
+        dialogOutlet: null,
+      },
+    },
+  ]);
+}
+
+
+}
