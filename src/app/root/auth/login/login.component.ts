0ebf170a69423b9f92379ff9d1baa909357c6048
Vlad <dog792804@gmail.com>
2023-03-15 15:38:38 +0200

    Fixed Comments

--- src/app/root/auth/login/login.component.ts
+++ src/app/root/auth/login/login.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject} from 'rxjs';
 import { InputTypes } from 'src/app/types/enums/input-types.enum';
-import { hashPassword } from '../passwordHash';
-import { samePasswords } from '../passwords.validator';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy{
+export class LoginComponent implements OnInit{
   
   private unsubscribe$ = new Subject();
 
@@ -26,23 +24,12 @@ export class LoginComponent implements OnInit, OnDestroy{
   }
 
   ngOnInit(): void {
-      this.LoginForm.get('repeatPassword')?.addValidators(samePasswords(this.LoginForm));
-      this.LoginForm.get('password')?.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
-          this.LoginForm.get('repeatPassword')?.updateValueAndValidity();
-      })
+      
   }
-
-
+  
   submitData() {
-      this.LoginForm.markAllAsTouched();
-
-      if(this.LoginForm.valid){
-          const valueToSend = JSON.parse(JSON.stringify(this.LoginForm.value));
-          delete valueToSend['repeatPassword'];
-          valueToSend.password = hashPassword(valueToSend.password);
-          console.log(valueToSend);
-      }
-}
+    this.LoginForm.markAllAsTouched();
+    }
 
 backToSignIn() {
   this.router.navigate([
@@ -60,9 +47,5 @@ backToSignIn() {
   ]);
 }
 
-ngOnDestroy(): void {
-    this.unsubscribe$.next(0);
-    this.unsubscribe$.complete();
-}
 
 }
